75fd4e3efbd5d8271cf16f3e8b58c6dcad664259
delacez <zephren123@gmail.com>
2019-10-16 14:21:52 -0700

    added src folder to maintian organization

---
+++ src/Order.java
@@ -0,0 +1,11 @@
+/**
+ * Zephren de la Cerda My Projects 4/24/2018
+ **/
+public class Order {
+
+    //add pizza
+    //remove pizza
+    //set name
+    //calculate total price
+    //print and reset for next order
+}
